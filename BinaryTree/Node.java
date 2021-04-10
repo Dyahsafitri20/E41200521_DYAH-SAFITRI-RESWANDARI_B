2e8df228096606ebcdeee288bd302573daa1997f
DYAH SAFITRI RESWANDARI <dyahsafitrii20@gmail.com>
2021-04-10 20:56:27 +0700

    four commit

---
+++ BinaryTree/Node.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BinaryTree;
+
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+    public void displayNode() {
+        System.out.print("( " + id + ", " + data + "}");
+    }
+}
